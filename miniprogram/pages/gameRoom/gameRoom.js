3f1b2d388b90890f224c76cb32d0876e24595558
SYQ0109 <SYQ0109@users.noreply.github.com>
2020-07-19 21:34:12 +0800

    绘画界面增加聊天室

--- miniprogram/pages/gameRoom/gameRoom.js
+++ miniprogram/pages/gameRoom/gameRoom.js
@@ -4,8 +4,23 @@ var systemInfo = wx.getSystemInfoSync();
 Page({
   data: {
     drawerOpenId: '',
+    onGetUserInfo: null,
+    getOpenID: null,
+    avatarUrl: './user-unlogin.png',
+    userInfo: null,
+    logged: false,
+    takeSession: false,
+    requestResult: '',
+    // chatRoomEnvId: 'release-f8415a',
+    chatRoomCollection: 'chatroom',
+    chatRoomGroupId: 'demo',
+    chatRoomGroupName: '聊天室',
+    // functions for used in chatroom components
+    onGetUserInfo: null,
+    getOpenID: null,
+
     selectorWidth:['超级细','细','适中','粗','超级粗'],
-    selectorColor:['黑色','蓝色','白色','黄色','绿色','红色']
+    selectorColor:['黑色','蓝色','白色','黄色','绿色','红色'],
   },
   selectorChange:function(e){
     let i = e.detail.value;
@@ -45,5 +60,62 @@ Page({
     this.setData({
       drawerOpenId: '',
     })
+  },
+  onLoad: function() {
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+
+    this.setData({
+      onGetUserInfo: this.onGetUserInfo,
+      getOpenID: this.getOpenID,
+    })
+
+    wx.getSystemInfo({
+      success: res => {
+        console.log('system info', res)
+        if (res.safeArea) {
+          const { top, bottom } = res.safeArea
+          this.setData({
+            containerStyle: `padding-top: ${(/ios/i.test(res.system) ? 10 : 20) + top}px; padding-bottom: ${20 + res.windowHeight - bottom}px`,
+          })
+        }
+      },
+    })
+  },
+
+  getOpenID: async function() {
+    if (this.openid) {
+      return this.openid
+    }
+
+    const { result } = await wx.cloud.callFunction({
+      name: 'login',
+    })
+
+    return result.openid
+  },
+
+  onGetUserInfo: function(e) {
+    if (!this.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo
+      })
+    }
   },
 })
