947a1155c47c47653d8ff765cffa3feba5516606
Robbery-Ibrahi <52434972+Robbery-Ibrahi@users.noreply.github.com>
2020-07-21 19:21:22 +0800

    排行榜功能完成；旁行榜界面大致实现

--- miniprogram/pages/ranklist/ranklist.js
+++ miniprogram/pages/ranklist/ranklist.js
@@ -1,66 +1,120 @@
 // miniprogram/pages/rankList/rankList.js
+var page = 1;
+var pre_ranking = [1, 2, 3, 4, 5];
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    ranking:[1, 2, 3, 4, 5],
+    ranklist: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function (element) {
+    this.onReady();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    wx.showLoading({
+      title: '玩命加载中',
+    })
+    page = 1;
+    pre_ranking = [1, 2, 3, 4, 5];
+    this.setData({
+      ranking:[1, 2, 3, 4, 5],
+      ranklist: []
+    })
+    this.getRankList(1, false);
+    wx.hideLoading();
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    page = 1;
+    pre_ranking = [1, 2, 3, 4, 5];
+    this.setData({
+      ranking:[1, 2, 3, 4, 5],
+      ranklist: []
+    })
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    wx.showNavigationBarLoading();
+    this.onLoad();
+    wx.hideNavigationBarLoading();
+    wx.stopPullDownRefresh();
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    wx.showLoading({
+      title: '玩命加载中',
+    })
+    page++;
+    this.getRankList(page, true);
+    wx.hideLoading();
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+    
+  },
 
-  }
+  getRankList: function(pagenum, flag){
+    wx.cloud.init({
+      traceUser: true
+    })
+    wx.cloud.callFunction({
+      name: 'get',
+      data: {
+        pageid: pagenum
+      },
+      complete: res => {
+        if (res){
+          this.setData({
+            ranklist: res.result.data,
+          })
+        }
+        if (flag){
+          pre_ranking[0] += 5;
+          pre_ranking[1] += 5;
+          pre_ranking[2] += 5;
+          pre_ranking[3] += 5;
+          pre_ranking[4] += 5;
+          this.setData({
+            ranking : pre_ranking
+          })
+        }
+      }
+    })    
+  },
 })
